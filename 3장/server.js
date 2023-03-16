91eccd78cc77b13bb5a6cdb09b6c064222d546a7
dntjdfo <dntjdfo55@gmail.com>
2023-03-17 00:08:31 +0900

    clear

--- 3장/server.js
+++ 3장/server.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 const _path = path.join(__dirname, "./dist");
 app.use("/", express.static(_path));
-app.listen(12011, () => {
-  console.log("lazy 이미지서버 : 12010시작 http://127.0.0.1:12011");
+app.listen(5000, () => {
+  console.log("lazy 이미지서버 : 12010시작 http://127.0.0.1:5000");
 });
